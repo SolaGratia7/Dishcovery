cd62f2ba52fda7af1671a9af857f4f0c9fa57163
joeyxy-08 <joey.saw.2024@computing.smu.edu.sg>
2025-11-06 16:26:30 +0800

    Added API keys

--- src/views/NutritionView.vue
+++ src/views/NutritionView.vue
@@ -373,7 +373,8 @@ const SPOONACULAR_API_KEY = [
   import.meta.env.VITE_SPOONACULAR_KEY_1,
   import.meta.env.VITE_SPOONACULAR_KEY_2,
   import.meta.env.VITE_SPOONACULAR_KEY_3,
-  import.meta.env.VITE_SPOONACULAR_KEY_4,  
+  import.meta.env.VITE_SPOONACULAR_KEY_4, 
+  import.meta.env.VITE_SPOONACULAR_KEY_5, 
 ]
 
 let currentKeyIndex = 0
